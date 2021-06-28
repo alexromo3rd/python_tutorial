955a8aac15387adc44bf099affef857b7293ffe4
Alex Romo III <alexromo3rd@gmail.com>
2021-06-27 23:15:40 -0500

    Working through tutorial

--- python_basics/basics.py
+++ python_basics/basics.py
@@ -1,7 +1,15 @@
-x = 10
-y = "10"
+# List
+# monday_temperatures = [9.1, 8.8, 7.5]
 
-sum1 = x + x
-sum2 = y + y
+# Dictionary
+# student_grades = {"Mary": 9.1, "Sim": 8.8, "John": 7.5}
 
-print(sum1, sum2)
+# Tuple
+monday_temperatures = (1, 4, 5)
+
+# my_sum = sum(student_grades.values())
+# length = len(student_grades)
+# mean = my_sum / length
+# print(mean)
+
+print(monday_temperatures)
