ff7baca67a268df9acbd44082fd499676faf4f02
Alex Romo III <alexromo3rd@gmail.com>
2021-06-29 17:56:32 -0500

    Dictionaries

--- python_basics/basics.py
+++ python_basics/basics.py
@@ -1,15 +1,15 @@
 # List
-# monday_temperatures = [9.1, 8.8, 7.5]
+monday_temperatures = [9.1, 8.8, 7.5]
 
 # Dictionary
 # student_grades = {"Mary": 9.1, "Sim": 8.8, "John": 7.5}
 
 # Tuple
-monday_temperatures = (1, 4, 5)
+# monday_temperatures = (1, 4, 5)
 
 # my_sum = sum(student_grades.values())
 # length = len(student_grades)
 # mean = my_sum / length
 # print(mean)
 
-print(monday_temperatures)
+# print(monday_temperatures)
